315ab6f66cf7f847b394666c8a6277eff8650a1a
eparadise <ecp25@duke.edu>
2020-06-20 18:38:20 -0400

    adding min and max zoom

--- meal-mapper/src/components/ResourceMap.vue
+++ meal-mapper/src/components/ResourceMap.vue
@@ -7,6 +7,8 @@
         :zoom="zoom"
         :center="center"
         :options="mapOptions"
+        :maxZoom="max"
+        :minZoom="min"
         style="height: 100%; width: 100%;"
         @update:center="centerUpdated"
         @update:zoom="(val) => (zoom = val)"
@@ -112,6 +114,8 @@ export default {
     return {
       center: latLng(this.centroid.lat, this.centroid.lng),
       zoom: this.centroid.zoom,
+      max: 17,
+      min: 12,
       showParagraph: true,
       showError: false,
       errorMessage: '',
