fd63637d41f314ea1ef05dd468cbbbc211aed4c5
eparadise <ecp25@duke.edu>
2020-06-24 14:40:13 -0400

    adding checkboxes and buttons to modal, still have to add submit functionality

--- meal-mapper/src/components/EditForm.vue
+++ meal-mapper/src/components/EditForm.vue
@@ -1,11 +1,22 @@
 <template>
-  <b-modal id="suggest-edit" size="lg" dialog-class="m-0 m-md-auto" centered scrollable hide-header-close>
+  <b-modal id="suggest-edit" size="lg" dialog-class="m-0 m-md-auto" centered scrollable>
     <template v-slot:modal-title>
       Suggest an edit
     </template>
     <p>
       Please use this form to let us know what is incorrect about the listing for <b>{{ currentBusiness.marker.gsx$mealsitename.$t }}.</b>
     </p>
+    <div>
+      <b-form-group>
+        <b-form-checkbox-group id="checkbox-group-1" v-model="selected" :options="options" name="flavour-1"></b-form-checkbox-group>
+      </b-form-group>
+    </div>
+    <template v-slot:modal-footer>
+      <div class="w-100">
+        <b-button variant="primary" class="float-right" @click="submitForm()">Submit</b-button>
+        <b-button variant="danger" class="float-left" @click="reset()">Reset</b-button>
+      </div>
+    </template>
   </b-modal>
 </template>
 
@@ -14,6 +25,27 @@ export default {
   name: 'suggest-edit-modal',
   props: {
     currentBusiness: Object
+  },
+  data() {
+    return {
+      selected: [],
+      options: [
+        { text: 'Site name', value: 'name' },
+        { text: 'Address', value: 'address' },
+        { text: 'Contact information', value: 'contact' },
+        { text: 'Social media', value: 'social' },
+        { text: 'Hours', value: 'hours' }
+      ]
+    }
+  },
+  methods: {
+    reset: function () {
+      this.selected = []
+    },
+    submitForm: function () {
+      alert('The form has been submitted.')
+      this.selected = []
+    }
   }
 }
 </script>
