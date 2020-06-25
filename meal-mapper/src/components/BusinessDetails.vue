73c394c1ad61b331d90b27049829a5325954c4b2
eparadise <ecp25@duke.edu>
2020-06-25 15:44:12 -0400

    fixing get directions

--- meal-mapper/src/components/BusinessDetails.vue
+++ meal-mapper/src/components/BusinessDetails.vue
@@ -18,7 +18,7 @@
           <p v-if="getAddress(business.marker) !== ''">
             <b>{{ $t('label.address') }}:</b><br />
             {{ getAddress(business.marker) }}
-            <icon-list-item icon="fa fa-directions" :title="'Get directions'" :link="directionsLink(addressURL(business.marker))" />
+            <icon-list-item icon="fa fa-directions" :title="$t('getdirections')" :link="directionsLink(addressURL(business.marker))" />
             <i class="fas fa-share-alt fa-lg" id="share-icon" aria-hidden="true" />
             <b-button variant="link" class="share-button" @click="$bvModal.show('share-location')">Share location</b-button>
           </p>
