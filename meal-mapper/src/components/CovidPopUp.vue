7b450a803d06bca1412960db56ec05a8922fd5f5
Helen Xiao <helen.xiao@duke.edu>
2020-08-04 14:22:12 -0700

    Basic popup, needs formatting

--- meal-mapper/src/components/CovidPopUp.vue
+++ meal-mapper/src/components/CovidPopUp.vue
@@ -1,12 +1,20 @@
 <template>
-  <div>
-    <b-modal ref="covid-popup" hide-footer title="Using Component Methods">
-      <div class="d-block text-center">
-        <h3>Please remember to wear a mask</h3>
-      </div>
-      <b-button class="mt-3" variant="outline-danger" block @click="hideModal">Close</b-button>
-    </b-modal>
-  </div>
+  <b-modal ref="covid-popup" size="l" dialog-class="m-0 m-md-auto" centered scrollable hide-footer>
+    <template v-slot:modal-title>
+      {{ $t('covidpopup.title') }}
+    </template>
+    <template v-slot:default>
+      <p id="wearmask">
+        <img src="../images/WearMask.png" alt="Wear A Mask" />
+      </p>
+      <p id="socialdist">
+        <img src="../images/SocialDistancing.png" alt="Social Distance" />
+      </p>
+      <p id="washhands">
+        <img src="../images/WashHands.png" alt="Wash Hands" />
+      </p>
+    </template>
+  </b-modal>
 </template>
 
 <script>
@@ -25,3 +33,27 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+#wearmask {
+  text-align: center;
+}
+#socialdist {
+  text-align: center;
+}
+#washhands {
+  text-align: center;
+}
+
+@media (max-width: 991px) {
+  #covid-popup > .modal-dialog {
+    justify-content: normal;
+    max-width: 100%;
+
+    & > .modal-content {
+      min-height: 100vh;
+      height: 100vh;
+    }
+  }
+}
+</style>
