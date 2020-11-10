bddee351d65432fac7c63fedf7a88cc2be110aa0
Nathan Ostrowski <nathancostrowski@gmail.com>
2020-11-09 23:31:27 -0500

    changed theme.header.vue to include a districtName prop and v-bind the img src to that prop

--- meal-mapper/src/themes/MealsForFamilies/components/theme.header.vue
+++ meal-mapper/src/themes/MealsForFamilies/components/theme.header.vue
@@ -1,14 +1,16 @@
 <template>
   <span class="pagetitle">
     <picture>
-      <source srcset="../logo-dark.png" media="(prefers-color-scheme: dark)" />
-      <img src="../logo.png" height="80" alt="Food For Students" />
+      <source :srcset="require(`@/districtLogos/${districtName}-logo-dark.png`)" media="(prefers-color-scheme: dark)" />
+      <img :src="require(`@/districtLogos/${districtName}-logo.png`)" height="80" alt="District Logo" />
     </picture>
   </span>
 </template>
 <script>
+
 export default {
-  name: 'theme-header'
+  name: 'theme-header',
+  props: ['districtName']
 }
 </script>
 <style lang="sass"></style>
