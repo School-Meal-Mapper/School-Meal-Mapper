5fd7434ac0825694d02f8055921ab7d454ed40d5
caleb-watson <caleb.watson@duke.edu>
2021-03-05 08:59:31 -0500

    styling changes for covid popup
    1. added text underneath each image
    2. changing styling of button to close the popup

--- meal-mapper/src/components/CovidPopUp.vue
+++ meal-mapper/src/components/CovidPopUp.vue
@@ -7,12 +7,24 @@
       <p id="wearmask">
         <i class="fas fa-head-side-mask" aria-hidden="true" style="font-size: 600%;"></i>
       </p>
+      <p id="text">
+        {{ $t('covidpopup.mask') }}
+      </p>
       <p id="socialdist">
         <i class="fas fa-people-arrows" aria-hidden="true" style="font-size: 600%;"></i>
+      </p>
+      <p id="text">
+        {{ $t('covidpopup.socialdist') }}
       </p>
       <p id="washhands">
         <i class="fas fa-hands-wash" aria-hidden="true" style="font-size: 600%;"></i>
       </p>
+      <p id="text">
+        {{ $t('covidpopup.washhands') }}
+      </p>
+      <b-button id="hide" size="md" @click="hideModal()">
+        Got it!
+      </b-button>
     </template>
   </b-modal>
 </template>
@@ -54,6 +66,24 @@ export default {
 #washhands {
   padding: 15px 0px 15px;
   text-align: center;
+}
+#text {
+  text-align: center;
+  font-weight: bold;
+}
+#hide {
+  font-weight: bold;
+  background-color: #1d6363;
+  color: white;
+  padding: 14px 28px;
+  font-size: 20px;
+  margin-left: 40%;
+}
+.close {
+  display: none;
+}
+.modal-title {
+  margin: 0 auto;
 }
 
 @media (max-width: 991px) {
