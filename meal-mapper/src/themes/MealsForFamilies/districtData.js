30ba1aad20ae8465d8e9555b233e9bd5f8012ba4
Kevin Chen <kekevi@github.com>
2021-06-04 15:58:30 -0400

    added images and district data for dublin usd

--- meal-mapper/src/themes/MealsForFamilies/districtData.js
+++ meal-mapper/src/themes/MealsForFamilies/districtData.js
@@ -90,6 +90,34 @@ if (url.searchParams.has('chccs') || hash == 'chccs') {
   darkUrl = 'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png'
   darkAttribution =
     '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+} else if (url.searchParams.has('dublinusd') || hash == 'dublinusd') { 
+  //case 'dusd':
+  // district name and theming
+  districtAbbr = 'dublinusd'
+  districtName = 'Dublin Unified School District'
+  primaryColor = '#005cba'
+  bannerColor = '#005cba'
+  bannerColorDark = '#005cba' // how do you get to dark mode?
+  accentColor = '#3e783a'
+  navLink = '#f8f8f8'
+  navLinkDark = '#f8f8f8'
+  // map settings for district-specific page
+  initialMapCenterLat = 37.713974
+  initialMapCenterLng = -121.920405
+  initialMapZoom = 12
+  clusterZoom = 13
+  maxZoom = 17
+  minZoom = 11
+  // data source urls and attributions
+  spreadsheetUrl = 'https://spreadsheets.google.com/feeds/list/1RxoLqeU-W785OZJICHMBD3tvZiTSL2vmRHtGE7-n29s/1/public/values?alt=json'
+  faqUrl = 'https://spreadsheets.google.com/feeds/list/1RxoLqeU-W785OZJICHMBD3tvZiTSL2vmRHtGE7-n29s/2/public/values?alt=json'
+  providerinfoUrl = 'https://spreadsheets.google.com/feeds/list/1RxoLqeU-W785OZJICHMBD3tvZiTSL2vmRHtGE7-n29s/3/public/values?alt=json'
+  lightUrl = 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager_labels_under/{z}/{x}/{y}.png'
+  lightAttribution =
+    '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+  darkUrl = 'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png'
+  darkAttribution =
+    '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 } /* else if (url.searchParams.has('cms') || hash == 'cms') {
   //case 'cms':
   // district name and theming
