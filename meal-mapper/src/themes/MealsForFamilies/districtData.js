f0aedc0a61242243ffa2417f75d968cadf1d99bb
Kevin Chen <kekevi@github.com>
2021-06-04 16:25:37 -0400

    added adding-dublinusd and fixed the prettier err

--- meal-mapper/src/themes/MealsForFamilies/districtData.js
+++ meal-mapper/src/themes/MealsForFamilies/districtData.js
@@ -90,7 +90,7 @@ if (url.searchParams.has('chccs') || hash == 'chccs') {
   darkUrl = 'https://{s}.basemaps.cartocdn.com/rastertiles/dark_all/{z}/{x}/{y}.png'
   darkAttribution =
     '&copy; <a href="https://carto.com/">Carto</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-} else if (url.searchParams.has('dublinusd') || hash == 'dublinusd') { 
+} else if (url.searchParams.has('dublinusd') || hash == 'dublinusd') {
   //case 'dusd':
   // district name and theming
   districtAbbr = 'dublinusd'
